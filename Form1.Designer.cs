ec63bc07d033d18a634b1b39288765d19ed598bb
Katja Papinniemi <Katja.Papinniemi@edu.savonia.fi>
2024-04-13 10:04:56 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace Tritex_ModBus
+{
+    partial class Tritex_Modbus
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Tritex_Modbus
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(533, 450);
+            this.Name = "Tritex_Modbus";
+            this.Text = "Tritex Modbus Controller";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
