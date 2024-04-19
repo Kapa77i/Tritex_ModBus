4d5e14946858feeb5214d503f8ed932e1fd55267
Katja Papinniemi <Katja.Papinniemi@edu.savonia.fi>
2024-04-19 20:06:28 +0300

    Both EasyModbus and Fluent code for test.

--- Modbus_Client.cs
+++ Modbus_Client.cs
@@ -9,6 +9,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using FluentModbus;
+using EasyModbus;
 
 // https://apollo3zehn.github.io/FluentModbus/
 
@@ -16,8 +17,8 @@ namespace Tritex_ModBus
 {
     public partial class Modbus_Client : Form
     {
-        //ModbusClient modbusClient;
-        ModbusTcpClient modbusClient;
+        EasyModbus.ModbusClient modbusClient;
+        //ModbusTcpClient modbusClient;
 
         public Modbus_Client()
         {
@@ -27,38 +28,86 @@ namespace Tritex_ModBus
         //Connection button functionality
         private void btnConnect_Click(object sender, EventArgs e)
         {
-            modbusClient = new ModbusTcpClient();
+            //modbusClient = new ModbusTcpClient();
 
-            //new IPEndPoint(IPAddress.Parse(cbEngines.Text), 502);
-            //new IPEndPoint(IPAddress.Parse("192.168.0.254"), 502); //Tritex IP?
+            ////new IPEndPoint(IPAddress.Parse(cbEngines.Text), 502);
+            ////new IPEndPoint(IPAddress.Parse("192.168.0.254"), 502); //Tritex IP?
+            ////Console.WriteLine(cbEngines.Text);
+            //new IPEndPoint(IPAddress.Parse("127.0.0.1"), 502);
+
+
+            //if (btnConnect.Text ==  "Connect")
+            //{
+            //    try
+            //    {
+            //        modbusClient.Connect(ModbusEndianness.BigEndian);
+            //        lbClientStatus.Text = "Client Connection: Connected!";
+            //        btnConnect.Text = "Disconnect";
+            //        btnConnect.BackColor = Color.Red;
+
+            //        //Enable jogs after succesfull connection
+            //        btnJogPlus.Enabled = true;
+            //        btnJobMinus.Enabled = true;
+            //        btnGoHome.Enabled = true;
+
+            //    }
+            //    catch (Exception ex)
+            //    {
+            //        lbClientStatus.Text = "ERROR! " + ex.ToString();
+            //    }
+            //}
+
+            //else if (btnConnect.Text == "Disconnect")
+            //{
+
+            //    try
+            //    {
+            //        modbusClient.Disconnect();
+            //        modbusClient = null;
+            //        lbClientStatus.Text = "Client Connection: Disconnected!";
+            //        btnConnect.Text = "Connect";
+            //        btnConnect.BackColor = Color.White;
+
+            //    }
+            //    catch (Exception ex)
+            //    {
+
+            //        lbClientStatus.Text = "ERROR! " + ex.ToString();
+            //    }
+            //}
+
+            modbusClient = new EasyModbus.ModbusClient();
+            //modbusClient.IPAddress = tbIP.Text;
+            //modbusClient.IPAddress = cbEngines.Text;
+            //modbusClient.IPAddress = "192.168.0.1";
+            modbusClient.IPAddress = "192.168.0.212";   //Tritex IP?
             //Console.WriteLine(cbEngines.Text);
-            new IPEndPoint(IPAddress.Parse("127.0.0.1"), 502);
-
-
-            if (btnConnect.Text ==  "Connect")
+            //modbusClient.IPAddress = "127.0.0.1";
+            modbusClient.IPAddress = "127.0.0.1";
+            modbusClient.Port = int.Parse(tbPort.Text);
+            Console.WriteLine(modbusClient.IPAddress);
+            Console.WriteLine(modbusClient.Port);
+            if (btnConnect.Text == "Connect")
             {
                 try
                 {
-                    modbusClient.Connect(ModbusEndianness.BigEndian);
+                    modbusClient.Available(500);
+                    modbusClient.Connect();
                     lbClientStatus.Text = "Client Connection: Connected!";
                     btnConnect.Text = "Disconnect";
                     btnConnect.BackColor = Color.Red;
-
                     //Enable jogs after succesfull connection
                     btnJogPlus.Enabled = true;
                     btnJobMinus.Enabled = true;
                     btnGoHome.Enabled = true;
-
                 }
                 catch (Exception ex)
                 {
                     lbClientStatus.Text = "ERROR! " + ex.ToString();
                 }
             }
-
             else if (btnConnect.Text == "Disconnect")
             {
-
                 try
                 {
                     modbusClient.Disconnect();
@@ -66,11 +115,9 @@ namespace Tritex_ModBus
                     lbClientStatus.Text = "Client Connection: Disconnected!";
                     btnConnect.Text = "Connect";
                     btnConnect.BackColor = Color.White;
-
                 }
                 catch (Exception ex)
                 {
-
                     lbClientStatus.Text = "ERROR! " + ex.ToString();
                 }
             }
@@ -79,38 +126,60 @@ namespace Tritex_ModBus
         //Jog movement forward
         private void btnJogPlus_Click(object sender, EventArgs e)
         {
+            ////Addressess
+            //var ADD_IEG_MOTION = 4317;  //0x10DD
+            //var ADD_JOG = 6020; //0x1784
+            //var ADD_FASTVEL = 6024;  // 0x1788, UVEL32
+            //var ADD_SLOWVELO = 6022; // 0x1786, UVEL32
+            //var ADD_ACCELERATION = 6026; // 0x178A, UACC32
+
+            ////Identifier
+            //var IEG_MOTION_ID = 0x10DD;
+            //short ADD_JOG_ID = 0x1784;
+
+            ////Default values to be used:
+            //int VAL_JOG = 65533; //# Max int value 65535. -1 if Alternate mode (AMO), -2 if default mode is on (DMO)
+            //int VAL_FASTVEL = 8;
+            //int VAL_SLOWVEL = 8;
+            //int VAL_ACCELERATION = 8;
+
+            //try
+            //{
+            //    modbusClient.WriteSingleRegister(ADD_JOG, VAL_JOG, ADD_JOG_ID);
+
+            //   // modbusClient.WriteSingleRegister(ADD_FASTVEL, VAL_FASTVEL);
+            //    //modbusClient.WriteSingleRegister(ADD_SLOWVELO, VAL_SLOWVEL);
+            //    //modbusClient.WriteSingleRegister(ADD_ACCELERATION, VAL_ACCELERATION);
+            //}
+            //catch (Exception ex)
+            //{
+
+            //    lbClientStatus.Text = "Error when writing! " + ex.ToString();
+            //}
+
             //Addressess
-            var ADD_IEG_MOTION = 4317;  //0x10DD
-            var ADD_JOG = 6020; //0x1784
-            var ADD_FASTVEL = 6024;  // 0x1788, UVEL32
-            var ADD_SLOWVELO = 6022; // 0x1786, UVEL32
-            var ADD_ACCELERATION = 6026; // 0x178A, UACC32
-
-            //Identifier
-            var IEG_MOTION_ID = 0x10DD;
-            short ADD_JOG_ID = 0x1784;
-
+            UInt16 ADD_IEG_MOTION = 4317;  //0x10DD
+            UInt16 ADD_JOG = 6020; //0x1784
+            UInt16 ADD_FASTVEL = 6024;  // 0x1788, UVEL32
+            UInt16 ADD_SLOWVELO = 6022; // 0x1786, UVEL32
+            UInt16 ADD_ACCELERATION = 6026; // 0x178A, UACC32
             //Default values to be used:
             int VAL_JOG = 65533; //# Max int value 65535. -1 if Alternate mode (AMO), -2 if default mode is on (DMO)
             int VAL_FASTVEL = 8;
             int VAL_SLOWVEL = 8;
             int VAL_ACCELERATION = 8;
-
             try
             {
-                modbusClient.WriteSingleRegister(ADD_JOG, VAL_JOG, ADD_JOG_ID);
-
-               // modbusClient.WriteSingleRegister(ADD_FASTVEL, VAL_FASTVEL);
-                //modbusClient.WriteSingleRegister(ADD_SLOWVELO, VAL_SLOWVEL);
-                //modbusClient.WriteSingleRegister(ADD_ACCELERATION, VAL_ACCELERATION);
+                modbusClient.WriteSingleRegister(ADD_JOG, VAL_JOG);
+                modbusClient.WriteSingleRegister(ADD_FASTVEL, VAL_FASTVEL);
+                modbusClient.WriteSingleRegister(ADD_SLOWVELO, VAL_SLOWVEL);
+                modbusClient.WriteSingleRegister(ADD_ACCELERATION, VAL_ACCELERATION);
             }
             catch (Exception ex)
             {
-
                 lbClientStatus.Text = "Error when writing! " + ex.ToString();
             }
 
-          
         }
     }
 }
--- Modbus_Server.cs
+++ Modbus_Server.cs
@@ -8,17 +8,20 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using FluentModbus;
+using EasyModbus;
 
 namespace Tritex_ModBus
 {
       public partial class Tritex_Modbus : Form
     {
-        ModbusTcpServer modbusServer;
+        //ModbusTcpServer modbusServer;
+        EasyModbus.ModbusServer modbusServer;
 
         public Tritex_Modbus()
         {
             InitializeComponent();
-            modbusServer = new ModbusTcpServer();
+            modbusServer = new EasyModbus.ModbusServer();
+            //modbusServer = new ModbusTcpServer();
             //modbusServer.LocalIPAddress = double.Parse("127.0.0.1");
 
             //Show the second form (client)
@@ -27,13 +30,12 @@ namespace Tritex_ModBus
 
         }
 
-
         private void btnStart_Click(object sender, EventArgs e)
         {
             if (btnStart.Text == "Start")
             {
-               
-                modbusServer.Start();
+
+                modbusServer.Listen();
                 lbStatus.Text = "Status of the Connection: Connected!";
                 btnStart.Text = "Stop";
                 btnStart.BackColor = Color.Red;
@@ -41,20 +43,52 @@ namespace Tritex_ModBus
             else if (btnStart.Text == "Stop")
             {
                 //modbusServer = new ModbusServer();
-                modbusServer.Dispose();
+                modbusServer.StopListening();
                 modbusServer = null;
                 lbStatus.Text = "Status of the Connection: Connection lost!";
                 btnStart.Text = "Start";
                 btnStart.BackColor = Color.White;
             }
         }
-
-        private void btnSet_Click(object sender, EventArgs e)
-        {
-            //if (cbRegisterType.Text == "Holding Register")
-            //{
-            //    ModbusServer.HoldingRegisters regs = modbusServer.holdingRegisters;
-            //}
-        }
+        //private void btnSet_Click(object sender, EventArgs e)
+        //{
+        //    if (cbRegisterType.Text == "Holding Register")
+        //    {
+        //        ModbusServer.HoldingRegisters regs = modbusServer.holdingRegisters;
+        //    }
+        //}
     }
+
+
+    //private void btnStart_Click(object sender, EventArgs e)
+    //{
+    //    if (btnStart.Text == "Start")
+    //    {
+
+    //        modbusServer.Start();
+    //        lbStatus.Text = "Status of the Connection: Connected!";
+    //        btnStart.Text = "Stop";
+    //        btnStart.BackColor = Color.Red;
+    //    }
+    //    else if (btnStart.Text == "Stop")
+    //    {
+    //        //modbusServer = new ModbusServer();
+    //        modbusServer.Dispose();
+    //        modbusServer = null;
+    //        lbStatus.Text = "Status of the Connection: Connection lost!";
+    //        btnStart.Text = "Start";
+    //        btnStart.BackColor = Color.White;
+    //    }
+    //}
+
+    //private void btnSet_Click(object sender, EventArgs e)
+    //{
+    //    //if (cbRegisterType.Text == "Holding Register")
+    //    //{
+    //    //    ModbusServer.HoldingRegisters regs = modbusServer.holdingRegisters;
+    //    //}
+    //}
+
+
 }
+
