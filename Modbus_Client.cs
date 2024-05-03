9e915c2e121d55c5c0433246ca1ed3ba994649da
Katja Papinniemi <Katja.Papinniemi@edu.savonia.fi>
2024-05-03 19:37:36 +0300

    Update

--- Modbus_Client.cs
+++ Modbus_Client.cs
@@ -133,13 +133,13 @@ namespace Tritex_ModBus
                 Console.WriteLine(modbusTcpClient1.IsConnected.ToString());
                 Console.WriteLine(modbusTcpClient2.IsConnected.ToString());
                 //IEG_MODE, enable
-                modbusTcpClient1.WriteSingleRegister(0x00, 4317, 2);
-                modbusTcpClient2.WriteSingleRegister(0x00, 4317, 2);
+                modbusTcpClient1.WriteSingleRegister(0x00, 4316, 2);
+                modbusTcpClient2.WriteSingleRegister(0x00, 4316, 2);
 
 
                 //IEG_MODE, Fault reset
-                modbusTcpClient1.WriteSingleRegister(0x00, 4317, 65535);
-                modbusTcpClient2.WriteSingleRegister(0x00, 4317, 65535);
+                modbusTcpClient1.WriteSingleRegister(0x00, 4316, 65535);
+                modbusTcpClient2.WriteSingleRegister(0x00, 4316, 65535);
                 lbClientStatus.Text = "Write succesful!";
 
                 btnEnable.Text = "Move Enabled!";
@@ -256,8 +256,8 @@ namespace Tritex_ModBus
             try
             {
                 //For move 2
-                modbusTcpClient1.WriteSingleRegister(0x00, 4322, 4);
-                modbusTcpClient2.WriteSingleRegister(0x00, 4322, 4);
+                modbusTcpClient1.WriteSingleRegister(0x00, 4322, 8);
+                modbusTcpClient2.WriteSingleRegister(0x00, 4322, 8);
                 lbClientStatus.Text = "Write succesful!";
 
             }
